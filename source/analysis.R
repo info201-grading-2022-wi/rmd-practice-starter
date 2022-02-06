73220d3965d2d7be824f51143b3172d510d38b73
dghendry <dhendry@uw.edu>
2022-02-05 18:01:55 -0800

    Update - ready for classroom

--- source/analysis.R
+++ source/analysis.R
@@ -3,25 +3,26 @@
 # Load the tidyverse package
 library(tidyverse)
 
-# Load the *national level* data into a variable. `national` 
+# Load the *national level* data into a variable. `national`
 national <- read.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv")
 
-# This function returns total cases in the US 
-get_total_cases <- function {
-total_cases <- national %>% 
-  filter(cases == max(cases)) %>%
-  pull(cases)
-return(total_cases)
+# This function returns total cases in the US
+get_total_cases <- function() {
+  total_cases <- national %>%
+    filter(cases == max(cases)) %>%
+    pull(cases)
+  return(prettyNum(total_cases,big.mark=",",scientific=FALSE))
 }
+
+?prettyNum()
 
 # This function returns the total number of cases in the US
-get_total_deaths <- function {
-total_deaths <- national %>% 
-  filter(deaths == max(deaths)) %>%
-  pull(deaths)
-return(total_deaths)
+get_total_deaths <- function() {
+  total_deaths <- national %>%
+    filter(deaths == max(deaths)) %>%
+    pull(deaths)
+  return(total_deaths)
 }
-
 
 # Run the following code to create a plot of cumulative cases over time
 # (we'll explain the ggplot2 syntax next week)
